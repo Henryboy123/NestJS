91abb9451128ee41207fbec6d6e8bc04bc8d4416
Haynrikh <haynrikh.santrosyan@optimumpartners.co>
2023-11-07 18:23:44 +0400

    proper migration table and data

--- db/migrations/1699283210417-CREATE_USER.ts
+++ db/migrations/1699283210417-CREATE_USER.ts
@@ -5,11 +5,17 @@ export class CREATEUSER1699283210417 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL DEFAULT ${this.name}, "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "testuser" ("id" SERIAL NOT NULL, "username" character varying NOT NULL DEFAULT 'testuser', "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+
+    await queryRunner.query(
+      `INSERT INTO "testuser" ("username", "password") VALUES
+       ('user1', 'password1'),
+       ('user2', 'password2')`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TABLE "testuser"`);
   }
 }
--- db/migrations/1699366834057-CREATE_TASK.ts
+++ db/migrations/1699366834057-CREATE_TASK.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class CREATETASK1699361676962 implements MigrationInterface {
+export class CREATETASK1699366834057 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
