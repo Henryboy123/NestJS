97442dd01baf0186c17860fe60bcd289669a1140
Haynrikh <haynrikh.santrosyan@optimumpartners.co>
2023-11-09 16:10:31 +0400

    adding cors functionality

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: ['http://localhost:3000'], // Replace with your allowed origins
+      methods: ['GET', 'PUT', 'POST', 'DELETE'], // Specify allowed HTTP methods
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
